2eebfad673a54ab0af5dba76de787f248b02e7de
kikrahau <vincent.koch@hotmail.com>
2015-08-16 19:20:15 +0200

    changed checkboxes for categories

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,14 @@
 module ApplicationHelper
 
-	def create_checkboxes_for klass
-		klass.map do |category|
-      content_tag(:div, class: 'checkbox') do
-        content_tag :label do
-          content_tag :input,category.name, type: 'checkbox',name: 'category_ids[]',value: category.id
-        end
+	def create_image_checkboxes_for klass
+		klass.map do |node|
+      array = []
+      array << content_tag(:input,nil, type: 'checkbox',name: 'category_ids[]',value: node.id, id: "_#{node.id}", class: 'image-checkbox')
+      array << content_tag(:label, for: "_#{node.id}") do
+      	image_tag node.image, class: 'checkbox-image'
       end
-    end.join.html_safe
+      array
+    end.flatten.join.html_safe
 	end
 
 end
